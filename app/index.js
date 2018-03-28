b1ebb23a07a3693a1fd05211033ddecaf121b5cc
Arie Visser <arie.visser@gmail.com>
2018-03-28 13:47:32 +0200

    Added validation for transaction request

--- app/index.js
+++ app/index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT
 const api = require('./api')
+const bodyParser = require('body-parser')
 
+app.use(bodyParser.json())
 app.use('/api/v1', api)
 
 app.listen(port, () => console.log(`API running on port ${port}`))
