59fbfb86260d70850ddd5914a3914bb626d0eafe
Arie Visser <arie.visser@gmail.com>
2018-03-28 11:23:05 +0200

    Added create transaction endpoint

--- app/api.js
+++ app/api.js
@@ -2,7 +2,9 @@ const express = require('express')
 const router = express.Router()
 const middleware = require('./middleware')
 const axios = require('axios')
+const bodyParser = require('body-parser')
 const StellarSdk = require('stellar-sdk')
+const jsonParser = bodyParser.json()
 const stellarServer = new StellarSdk.Server('https://horizon-testnet.stellar.org')
 
 router.use(middleware)
@@ -48,8 +50,64 @@ router.get('/asset', (req, res) => {
     res.send('GET asset')
 })
 
-router.post('/transaction', (req, res) => {
-    res.send('POST create transaction')
+router.post('/transaction', jsonParser, (req, res) => {
+    if (!req.body) {
+        return res.sendStatus(400)
+    }
+
+    // Todo: Validate properly
+    if (!req.body.secret) { // String
+        return res.status(422).send('The origin secret is missing.')
+    }
+
+    if (!req.body.destination) { // String
+        return res.status(422).send('The destination id is missing.')
+    }
+
+    if (!req.body.amount) { // String
+        return res.status(422).send('The amount is missing.')
+    }
+
+    if (!req.body.memo) { // String
+        return res.status(422).send('The memo is missing.')
+    }
+
+    const sourceKeys = StellarSdk.Keypair.fromSecret(req.body.secret)
+    const destinationId = req.body.destination
+    let transaction
+
+    StellarSdk.Network.useTestNetwork()
+
+    stellarServer.loadAccount(destinationId)
+    // Destination account not found
+    .catch(StellarSdk.NotFoundError, (error) => {
+        return res.status(422).send('The destination account does not exist.')
+    })
+    // Load origin account
+    .then(() => {
+        return stellarServer.loadAccount(sourceKeys.publicKey());
+    })
+    .then((originAccount) => {
+        transaction = new StellarSdk.TransactionBuilder(originAccount)
+        .addOperation(StellarSdk.Operation.payment({
+            destination: destinationId,
+            asset: StellarSdk.Asset.native(), // Lumens
+            amount: req.body.amount
+        }))
+        // Add meta data
+        .addMemo(StellarSdk.Memo.text(req.body.memo))
+        .build()
+
+        transaction.sign(sourceKeys);
+
+        return stellarServer.submitTransaction(transaction);
+    })
+    .then((result) => {
+        return res.send(`Transaction ${result.hash} sucessful.`)
+    })
+    .catch((err) => {
+        return res.status(500).send(`Stellar exception. ${err.toString()}`)
+    });
 })
 
 module.exports = router
--- app/middleware.js
+++ app/middleware.js
@@ -2,7 +2,7 @@ const apiKey = process.env.API_KEY
 
 function checkToken(req, res, next) {
     if (req.get('X-API-KEY') !== apiKey) {
-        return res.status(401).send('Unauthorized')
+        return res.status(401).send('Unauthorized.')
     }
 
     next()
