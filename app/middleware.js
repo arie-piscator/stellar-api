2339f9ea6c5789d5fa6b28a21802c94f97259a5a
Arie Visser <arie.visser@gmail.com>
2018-04-04 15:50:53 +0200

    Middleware cleanup

--- app/middleware.js
+++ app/middleware.js
@@ -1,11 +1,9 @@
 const apiKey = process.env.API_KEY
 
-function checkToken(req, res, next) {
+module.exports = (req, res, next) => {
     if (req.get('X-API-KEY') !== apiKey) {
         return res.status(401).send('Unauthorized.')
     }
 
     next()
 }
-
-module.exports = checkToken
