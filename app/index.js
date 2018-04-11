3c1b13569c57ee50cd67b90cb3048f57062d60ce
Arie Visser <arie.visser@gmail.com>
2018-04-11 15:35:08 +0200

    Stellar Live account creation and cors

--- app/index.js
+++ app/index.js
@@ -1,8 +1,15 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = process.env.PORT
 const api = require('./api')
 const bodyParser = require('body-parser')
+
+if (process.env.STELLAR_NETWORK === 'live') {
+    app.use(cors({
+        origin: /pro\.stemapp\.io$/
+    }))
+}
 
 app.use(bodyParser.json())
 app.use('/api/v1', api)
--- app/resources/stellar/account.js
+++ app/resources/stellar/account.js
@@ -2,6 +2,7 @@ const axios = require('axios')
 const stellarSdk = require('stellar-sdk')
 const stellarServer = new stellarSdk.Server(process.env.STELLAR_SERVER)
 const exception = require('../../exception')
+const stellarTransaction = require('./transaction')
 
 module.exports = {
     get: (account) => {
@@ -55,10 +56,27 @@ module.exports = {
         return new Promise((resolve, reject) => {
             const pair = stellarSdk.Keypair.random();
 
-            // Todo: send 1 XLM to account
-            resolve({
-                publicKey: pair.publicKey(),
-                secret: pair.secret()
+            // Send 2 XLM to account in order to create it and provide
+            // enough balance for future transactions.
+            stellarTransaction.create(
+                process.env.STELLAR_SECRET,
+                pair.publicKey(),
+                '2',
+                false,
+                false,
+                'Base fees'
+            )
+            .then(result => {
+                resolve({
+                    publicKey: pair.publicKey(),
+                    secret: pair.secret()
+                })
+            })
+            .catch(err => {
+                reject({
+                    status: err.status,
+                    message: err.message
+                })
             })
         })
 
