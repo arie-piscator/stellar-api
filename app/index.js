64a8b0d66cbf3c6ac449fe8eeb55d7a709027422
Arie Visser <arie.visser@gmail.com>
2018-04-16 14:16:33 +0200

    IPfilter

--- app/index.js
+++ app/index.js
@@ -1,14 +1,20 @@
 const express = require('express')
-const cors = require('cors')
 const app = express()
 const port = process.env.PORT
 const api = require('./api')
 const bodyParser = require('body-parser')
+const ip = require('express-ipfilter');
+
+const ips = ['::1'];
 
 if (process.env.STELLAR_NETWORK === 'live') {
-    app.use(cors({
-        origin: [/localhost$/,  /pro\.stemapp\.io$/]
-    }))
+    app.use(ip.IpFilter(ips, {mode: 'allow'}))
+
+    app.use((err, req, res, next) => {
+        if (err instanceof ip.IpDeniedError) {
+            return res.status(401).send('Access denied for IP')
+        }
+    })
 }
 
 app.use(bodyParser.json())
