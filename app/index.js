5034e1537f114b68c25f3adfefb6c10c9b77efca
Arie Visser <arie.visser@gmail.com>
2018-04-16 14:24:55 +0200

    Removed ipfilter

--- app/index.js
+++ app/index.js
@@ -3,19 +3,6 @@ const app = express()
 const port = process.env.PORT
 const api = require('./api')
 const bodyParser = require('body-parser')
-const ip = require('express-ipfilter');
-
-const ips = ['185.127.108.21', '::1'];
-
-if (process.env.STELLAR_NETWORK === 'test') {
-    app.use(ip.IpFilter(ips, {mode: 'allow'}))
-
-    app.use((err, req, res, next) => {
-        if (err instanceof ip.IpDeniedError) {
-            return res.status(401).send('Access denied for IP')
-        }
-    })
-}
 
 app.use(bodyParser.json())
 app.use('/api/v1', api)
